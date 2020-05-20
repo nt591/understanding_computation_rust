675eddc476a590012bbf729277b2e5371010dbbf
Nikhil Thomas <nikhil@arceoanalytics.com>
2020-05-20 14:29:47 -0400

    with formatting
    Signed-off-by: Nikhil Thomas <nikhil@arceoanalytics.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 enum SimpleType {
-    Number { value: i64 },
-    Add { left: Box<SimpleType>, right: Box<SimpleType> },
-    Multiply { left: Box<SimpleType>, right: Box<SimpleType> },
+    Number {
+        value: i64,
+    },
+    Add {
+        left: Box<SimpleType>,
+        right: Box<SimpleType>,
+    },
+    Multiply {
+        left: Box<SimpleType>,
+        right: Box<SimpleType>,
+    },
 }
 
 trait Showable {
@@ -15,9 +23,9 @@ trait Reducible {
 impl Showable for SimpleType {
     fn show(&self) -> String {
         match self {
-            SimpleType::Number { value }=> value.to_string(),
-            SimpleType::Add { left, right }=> format!("{} + {}", left.show(), right.show()),
-            SimpleType::Multiply { left, right }=> format!("{} * {}", left.show(), right.show())
+            SimpleType::Number { value } => value.to_string(),
+            SimpleType::Add { left, right } => format!("{} + {}", left.show(), right.show()),
+            SimpleType::Multiply { left, right } => format!("{} * {}", left.show(), right.show()),
         }
     }
 }
@@ -26,15 +34,21 @@ impl Reducible for SimpleType {
     fn reduce(&self) -> i64 {
         match self {
             SimpleType::Number { value } => *value,
-            SimpleType::Add  { left, right } => left.reduce() + right.reduce(),
-            SimpleType::Multiply  { left, right } => left.reduce() * right.reduce(),
+            SimpleType::Add { left, right } => left.reduce() + right.reduce(),
+            SimpleType::Multiply { left, right } => left.reduce() * right.reduce(),
         }
     }
 }
 
 fn main() {
-    let m1 = SimpleType::Multiply { left: Box::new(SimpleType::Number { value:  4 }), right: Box::new(SimpleType::Number { value:  8 }) };
-    let add = SimpleType::Add { left: Box::new(SimpleType::Number { value:  10 }), right: Box::new(m1) };
+    let m1 = SimpleType::Multiply {
+        left: Box::new(SimpleType::Number { value: 4 }),
+        right: Box::new(SimpleType::Number { value: 8 }),
+    };
+    let add = SimpleType::Add {
+        left: Box::new(SimpleType::Number { value: 10 }),
+        right: Box::new(m1),
+    };
 
     println!("{}", add.show());
     println!("Value: {}", add.reduce());
