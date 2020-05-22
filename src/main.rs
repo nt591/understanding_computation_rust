0975c8c1c337353055228aa1bc6a160c47dc8c33
Nikhil Thomas <nikhil@arceoanalytics.com>
2020-05-22 12:29:55 -0400

    shout out to discord
    Signed-off-by: Nikhil Thomas <nikhil@arceoanalytics.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
+struct BinaryOp {
+    left: SimpleType,
+    right: SimpleType
+}
 enum SimpleType {
-    Number {
-        value: i64,
-    },
-    Add {
-        left: Box<SimpleType>,
-        right: Box<SimpleType>,
-    },
-    Multiply {
-        left: Box<SimpleType>,
-        right: Box<SimpleType>,
-    },
+    Number(i64),
+    Add(Box<BinaryOp>),
+    Multiply(Box<BinaryOp>),
 }
 
 trait Reducible {
@@ -20,9 +16,9 @@ trait Reducible {
 impl std::fmt::Display for SimpleType {
     fn fmt(&self,  f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            SimpleType::Number { value } => write!(f, "{}", value),
-            SimpleType::Add { left, right } => write!(f, "{} + {}", left, right),
-            SimpleType::Multiply { left, right } => write!(f, "{} * {}", left, right),
+            SimpleType::Number(val) => write!(f, "{}", val),
+            SimpleType::Add(val) => write!(f, "{} + {}", val.left, val.right),
+            SimpleType::Multiply(val) => write!(f, "{} * {}", val.left, val.right),
         }
     }
 }
@@ -30,25 +26,25 @@ impl std::fmt::Display for SimpleType {
 impl Reducible for SimpleType {
     fn inner_reduce(&self) -> i64 {
         match self {
-            SimpleType::Number { value } => *value,
-            SimpleType::Add { left, right } => left.inner_reduce() + right.inner_reduce(),
-            SimpleType::Multiply { left, right } => left.inner_reduce() * right.inner_reduce(),
+            SimpleType::Number(val) => *val,
+            SimpleType::Add(val) => val.left.inner_reduce() + val.right.inner_reduce(),
+            SimpleType::Multiply(val) => val.left.inner_reduce() * val.right.inner_reduce(),
         }
     }
     fn reduce(&self) -> SimpleType {
-        SimpleType::Number { value: self.inner_reduce() }
+        SimpleType::Number(self.inner_reduce())
     }
 }
 
 fn main() {
-    let m1 = SimpleType::Multiply {
-        left: Box::new(SimpleType::Number { value: 4 }),
-        right: Box::new(SimpleType::Number { value: 8 }),
-    };
-    let add = SimpleType::Add {
-        left: Box::new(SimpleType::Number { value: 10 }),
-        right: Box::new(m1),
-    };
+    let m1 = SimpleType::Multiply(Box::new(BinaryOp {
+        left: SimpleType::Number(4),
+        right: SimpleType::Number(8),
+    }));
+    let add = SimpleType::Add(Box::new(BinaryOp {
+        left: SimpleType::Number(10),
+        right: m1,
+    }));
 
     println!("{}", add);
     println!("Value: {}", add.reduce());
